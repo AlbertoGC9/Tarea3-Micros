eb84ef2964d6d84baaa644f9eca7152e4705631e
murilloAD <70825503+murilloAD@users.noreply.github.com>
2020-10-17 09:34:14 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup tools import setup
+
+setup(
+  # Descripcion
+  name = 'PaqueteTarea3'
+  url = 
+  author = 'A. Guillen; E. Montero; A. Murillo; E. Salazar'
+  # Comandos
+  packages = ['PaquteTarea3']
+  install_requires = ['','']
