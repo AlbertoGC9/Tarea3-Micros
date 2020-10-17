639e9cb8da9ff6b3a3bc57ab8395aec3bd243676
murilloAD <70825503+murilloAD@users.noreply.github.com>
2020-10-17 09:52:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setup tools import setup
 
 setup(
   # Descripcion
-  name = 'PaqueteTarea3'
-  url = 
-  author = 'A. Guillen; E. Montero; A. Murillo; E. Salazar'
-  # Comandos
-  packages = ['PaquteTarea3']
-  install_requires = ['pygame','PIL']
+  name = 'PaqueteTarea3',
+  url = 'https://github.com/AlbertoGC9/Tarea3-Micros',
+  author = 'A. Guillen; E. Montero; A. Murillo; E. Salazar',
+  author_email = 'N/A',
+  # Comandos instalacion
+  packages = ['PaquteTarea3'],
+  install_requires = ['pygame','PIL'],
+  version = '1.0',
+  license = 'N/A',
+  description = 'Paquete para Tarea 3 del curso de Microprocesadores y microcontroladores'
